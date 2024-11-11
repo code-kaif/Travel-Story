92966c6fe1ba9ba3201a4fa1fd93e91d79cd556b
Kaif <kaif98747@gmail.com>
2024-11-11 13:13:50 +0530

    change url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,6 @@ import App from "./App.jsx";
 import "react-day-picker/style.css";
 import "./index.css";
 
-export const url = "http://localhost:5000";
+export const url = "https://travel-story-89n3.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
