ef20e7b41417fefb0ea12173e74e9ba68258aa43
Kaif <kaif98747@gmail.com>
2024-11-27 16:06:34 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "https://travel-story-virid.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
